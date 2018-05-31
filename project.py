8d9c6b38f0efe3688387bcf1eb05b38558567614
Shaik Khadar Pasha <skkhadarpasha8@gmail.com>
2018-05-31 11:33:15 -0500

    Update project.py

--- project.py
+++ project.py
@@ -5,6 +5,9 @@ from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 
+from flask import session as login_session
+import random, string
+
 
 #Connect to Database and create database session
 engine = create_engine('sqlite:///restaurantmenu.db')
@@ -13,6 +16,14 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+#for to prevent reuest forgery
+
+@app.route('/login')
+def showLogin();
+     state = ''.join(random.choice(string.ascii_uppercase + string.
+           digits) for x in xrange(32))
+    login_session['state'] = state
+    return "the current session state is %s" %login_session['state']
 
 #JSON APIs to view Restaurant Information
 @app.route('/restaurant/<int:restaurant_id>/menu/JSON')
